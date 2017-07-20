31b172cd33eb79f345f52b00eeb54a94542b0199
Brandon Gannicott <brandon.gannicott@gmail.com>
2017-07-19 21:38:11 -0500

    Add rudimentary hand and drawing action

--- deck.js
+++ deck.js
@@ -1,9 +1,7 @@
-
-
 var Card = function(text) {
   return {
     $type: 'div',
-    style: 'width: 50px;border:1px solid black;',
+    class: 'card',
     $text: text
   }
 }
@@ -16,11 +14,21 @@ Deck = function(sampleCards) {
     _count() { return this._cards.length },
     $init() { 
       this._cards = sampleCards.map(Card);
-      this.$components.push({ id: 'deck-counter', $text: this._count()})
+      this.$components.push({ id: 'deck-counter', $text: this._count(), class: 'card'})
     },
-    onclick() { if(this._count() > 0) { this.$components.push(this._cards.pop()) } },
+    onclick() { if(this._count() > 0) { document.querySelector('#hand')._add(this._cards.pop()) } },
     $update() { this.querySelector('#deck-counter').$text = this._count()},
     $components: []
+  }
+}
 
+Hand = function() {
+  return {
+    $cell: true,
+    id: 'hand',
+    style: 'height: 100px; padding: 10px; border:1px dashed black; display: flex',
+    _items: [],
+    _add(card) { this._items.push(card) },
+    $update() { this.$components = this._items;}
   }
 }
